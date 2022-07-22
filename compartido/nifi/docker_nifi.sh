532f88a5848ab90780a1a8895b34621faa2e90a2
Valentina Novo Villa <novovillavalentina@gmail.com>
2022-07-22 19:22:55 -0300

    Contraseñas dinámicas, falta correr nifi en HTTPS

--- compartido/nifi/docker_nifi.sh
+++ compartido/nifi/docker_nifi.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 # -*- ENCODING: UTF-8 -*-
-sudo docker-compose up --scale nifi=3 -d
-sudo docker-compose start 
 
+read -p"Ingrese nombre de usuario" USER_ACC
+read -p"Ingrese una contraseña" USER_PAS
+export USER_ACCESS=$(echo $USER_ACC)
+export USER_PASS=$(echo $USER_PAS)
+
+
+docker run --name nifi \
+  -p 8443:8443 \
+  -d \
+  -e SINGLE_USER_CREDENTIALS_USERNAME=${USER_ACCESS} \
+  -e SINGLE_USER_CREDENTIALS_PASSWORD=${USER_PASS} \
+  apache/nifi:latest
 exit
