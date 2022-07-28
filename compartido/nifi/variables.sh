e0f65657c1527fdc0335f20cc71d51e887ccf65a
root <root@hexacta.com>
2022-07-28 18:22:54 +0000

    Modificación en la creación de archivo .env

--- compartido/nifi/variables.sh
+++ compartido/nifi/variables.sh
@@ -6,6 +6,6 @@ export USER_ACCESS=$(echo $USER_ACC)
 export USER_PASS=$(echo $USER_PAS)
 export IP_ADRESS=$(ip -o -4 addr list eth0 |awk '{print $4}' |cut -d / -f1)
 destdir=./.env
-echo "USER_ACCESS="$USER_ACCESS"" >> "$destdir"
+echo "USER_ACCESS="$USER_ACCESS"" > "$destdir"
 echo "USER_PASS="$USER_PASS"" >> "$destdir"
 echo "IP_AD="$IP_ADRESS"" >> "$destdir"
