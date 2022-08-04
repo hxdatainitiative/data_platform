5c17d6fa227d85a81d40ca41deba878fda4d2679
FacuTadic <facutadich@gmail.com>
2022-08-04 16:57:39 -0300

    Primer modelado de solucion

---
+++ windows/scripts/GE-installScipt.py
@@ -0,0 +1,10 @@
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+
+if __name__ == '__main__':
+    install('great_expectations')
+
